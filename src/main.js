ee9560b719cdc90a49c202b0c12e3deebca63785
tinearconn <etienne.caron.pro@gmail.com>
2015-12-12 22:29:00 +0000

    Adding functions prototype

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-
 // Taille du jeu
 const width = 400;
 const height = 600;
 
 var gButton, nButton;
 var gButtonSprite, nButtonSprite, mainTitle, textGrown, textNext, textCreated, textLudum;
-var scoring, lifeText, life1, life2, life3, timerText, timer, seed;
+var scoring, score;
+var lifeText, life1, life2, life3;
+var timerText, timer, seed, seedParam, phase;
+var itTimeToBegin = true;
 
 // Phaser
 var game = new Phaser.Game(width, height, Phaser.AUTO, 'game', {
@@ -25,10 +27,10 @@ function preload() {
     game.load.image('buttonN', 'img/buttonN.png');
     game.load.image('background', 'img/background.png');
     game.load.image('howitzer', 'img/howitzer.png');
-    
-    game.load.audio('soundG',['sounds/gButton.MP3']);
-    game.load.audio('soundN',['sounds/nButton.MP3']);
-    
+
+    game.load.audio('soundG', ['sounds/gButton.MP3']);
+    game.load.audio('soundN', ['sounds/nButton.MP3']);
+
 }
 
 function create() {
@@ -67,24 +69,41 @@ function create() {
 
     game.soundG = game.add.audio('soundG');
     game.soundN = game.add.audio('soundN');
+
 }
 
 function update() {
 
-    if (gButton.isDown) {
+    if (gButton.isDown && gButton.downDuration(50)) {
         game.soundG.play();
-        beginGame();
+
+        if (itTimeToBegin) {
+            beginGame();
+        }
+        else {
+            grown();
+        }
     }
 
-    if (nButton.isDown) {
+    if (nButton.isDown && nButton.downDuration(50)) {
         game.soundN.play();
-        beginGame();
+
+        if (itTimeToBegin) {
+            beginGame();
+        }
+        else {
+            next();
+        }
     }
 
 }
 
-function beginGame(){
-    
+function beginGame() {
+
+    itTimeToBegin = false;
+    score = 0;
+    phase = 0;
+
     // Suppression de l'affichage du menu
     gButtonSprite.destroy();
     nButtonSprite.destroy();
@@ -93,32 +112,116 @@ function beginGame(){
     textGrown.destroy();
     textLudum.destroy();
     textNext.destroy();
-    
+
     // Affichage des params du jeu
-    lifeText = game.add.text(150, 120, "Life :", {
+    lifeText = game.add.text(145, 130, "Life :", {
         font: "28px Arial",
         fill: "#ffffff"
     });
-    life1 = game.add.sprite(220, 100, 'scythe');
-    life2 = game.add.sprite(260, 100, 'scythe');
-    life3 = game.add.sprite(300, 100, 'scythe');
+    life1 = game.add.sprite(220, 110, 'scythe');
+    life2 = game.add.sprite(260, 110, 'scythe');
+    life3 = game.add.sprite(300, 110, 'scythe');
     life1.scale.setTo(3, 3);
     life2.scale.setTo(3, 3);
     life3.scale.setTo(3, 3);
 
-    timerText = game.add.text(180, 220, "Timer : 5.0", {
+    timerText = game.add.text(180, 200, "Timer : 5.0", {
         font: "28px Arial",
         fill: "#ffffff"
     });
 
     //game.time.events.loop(Phaser.Timer, updateTimer, this);
-    
-    scoring = game.add.text(width / 2, height - 20, "0", {
-        font: "12px Arial",
+
+    scoring = game.add.text(180, 250, "Score : 0", {
+        font: "28px Arial",
         fill: "#ffffff"
     });
-    
+
+    addNewSeed();
+
 }
+
+function addNewSeed() {
+
+    //On récupère aléatoirement une graine selon la phase
+    seed = game.add.sprite(width / 2 - 10, height - 150, 'seed1');
+    seedParam = {
+        life: 1,
+        grown: 0,
+        bomb: false
+    };
+    seed.scale.setTo(2, 2);
+};
+
+function grown() {
+
+    seedParam.grown++;
+    if (seedParam.bomb) losingLife(3);
+    if (seedParam.grown > seedParam.life) {
+        losingLife(1);
+    }
+};
+
+function next() {
+
+    if (seedParam.grown === seedParam.life) {
+        scoring();
+        phase++;
+        addNewSeed();
+    }
+    else {
+        losingLife(1);
+    }
+};
+
+function losingLife(lose) {
+
+    if (lose === 3) {
+        life3.destroy();
+        life2.destroy();
+        gameOver();
+    }
+    else {
+        if (life3.visible) {
+            life3.destroy();
+        }
+        else if (life2.visible) {
+            life2.destroy();
+        }
+        else {
+            gameOver();
+        }
+    }
+};
+
+function scoring() {
+    switch (seed.life) {
+        case 1:
+            score += 100;
+            break;
+        case 2:
+            score += 200;
+            break;
+        case 3:
+            score += 500;
+            break;
+        case 4:
+            score += 1000;
+            break;
+        case 5:
+            score += 2000;
+            break;
+    }
+    scoring.setText('Score : ' + score);
+};
+
+function gameOver() {
+
+    lifeText.setText('GAME OVER');
+    life1.destroy();
+    timerText.destroy();
+
+};
 
 // function updateTimer() {
 
