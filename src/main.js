1ee74cc52ef2573817827674ec4c449577640274
tinearconn <etienne.caron.pro@gmail.com>
2015-12-12 17:26:38 +0000

    Prototype Menu

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ var GAME_OVER = false;
 
 // Taille du jeu
 const width = 400;
-const height = 1000;
+const height = 600;
+
+var gButton, nButton, gButtonSprite, nButtonSprite;
 
 // Phaser
 var game = new Phaser.Game(width, height, Phaser.AUTO, 'game', {
@@ -12,9 +14,6 @@ var game = new Phaser.Game(width, height, Phaser.AUTO, 'game', {
     create: create,
     update: update
 });
-
-// On rend le background transparent
-game.transparent = true;
 
 function preload() {
     game.load.image('seed1', 'img/seed1.png');
@@ -30,12 +29,51 @@ function preload() {
 }
 
 function create() {
-    
-    //game.stage.background = game.add.sprite(0,0,'background');
-    game.stage.background = '#AAA5FF';
-    game.stage.background.width = width;
-    game.stage.background.height = height;
-    
+
+    game.stage.background = game.add.sprite(0, 0, 'background');
+    game.stage.background.scale.setTo(4, 4);
+    game.transparent = true;
+
+    gButton = game.input.keyboard.addKey(Phaser.Keyboard.g);
+    nButton = game.input.keyboard.addKey(Phaser.Keyboard.n);
+
+    game.add.text(width - 200, 120, "Ultimate \nFarming \nSimulator", {
+        font: "40px Arial",
+        fill: "#ffffff"
+    });
+    game.add.text(120, height - 40, "Created by Tine Arconn", {
+        font: "14px Arial",
+        fill: "#ffffff"
+    });
+    game.add.text(60, height - 20, "Ludum Dare 34 - Growing & Two Buttons Controls", {
+        font: "12px Arial",
+        fill: "#ffffff"
+    });
+
+    gButtonSprite = game.add.sprite(50, height - 130, 'buttonG');
+    game.add.text(80, height - 130, "Grown", {
+        font: "30px Arial",
+        fill: "#ffffff"
+    });
+
+    nButtonSprite = game.add.sprite(230, height - 130, 'buttonN');
+    game.add.text(260, height - 130, "Next", {
+        font: "30px Arial",
+        fill: "#ffffff"
+    });
+
+    //game.events.on
+
 }
 
-function update() {}
+function update() {
+
+    if (gButton.isDown) {
+
+    }
+
+    if (nButton.isDown) {
+
+    }
+
+}
