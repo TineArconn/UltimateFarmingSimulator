b346471b1b1a34a84caf6ea7d694bc75212a6319
caron <etienne.caron@inist.fr>
2017-11-10 18:30:06 +0100

    WIP : isometric farm

--- src/states/Splash.js
+++ src/states/Splash.js
@@ -11,18 +11,34 @@ export default class extends Phaser.State {
     centerGameObjects([this.loaderBg, this.loaderBar])
     this.load.setPreloadSprite(this.loaderBar)
 
-    // Load your assets
+    // Tiles
     this.load.image('seed1', './assets/images/tiles/seed1.png')
     this.load.image('seed2', './assets/images/tiles/seed2.png')
     this.load.image('seed3', './assets/images/tiles/seed3.png')
     this.load.image('seed4', './assets/images/tiles/seed4.png')
     this.load.image('seed5', './assets/images/tiles/seed5.png')
     this.load.image('scythe', './assets/images/marker/scythe.png')
+
+    this.load.image('tile1', './assets/images/tiles/grass3.png')
+    this.load.image('tile2', './assets/images/tiles/iso-grass2.png')
+    this.load.image('tile3', './assets/images/tiles/richsoil-farm.png')
+    this.load.image('tile4', './assets/images/tiles/iso-dirt.png')
+    this.load.image('tile5', './assets/images/tiles/iso-dirt-border.png')
+
+    // Buttons
     this.load.image('buttonG', './assets/images/keyboard/buttonG.png')
     this.load.image('buttonN', './assets/images/keyboard/buttonN.png')
-    this.load.image('background', './assets/images/background/background.png')
+
     this.load.image('howitzer', './assets/images/tiles/howitzer.png')
 
+    // Background
+    this.load.image('stables', './assets/images/background/stables.svg')
+    this.load.image('warehouse', './assets/images/background/warehouse.svg')
+    this.load.image('windmill', './assets/images/background/windmill.svg')
+    this.load.image('well', './assets/images/background/well.svg')
+
+
+    // Sound
     this.load.audio('soundG', ['./assets/sound/gButton.MP3'])
     this.load.audio('soundN', ['./assets/sound/nButton.MP3'])
   }
--- src/states/Title.js
+++ src/states/Title.js
@@ -3,15 +3,19 @@ import Phaser from 'phaser'
 import styles from '../interfaces/styles.js'
 export default class extends Phaser.State {
   init () {}
-  preload () {}
+  preload () {
+    this.isoPlugin = this.game.plugins.add(Phaser.Plugin.Isometric)
+    this.game.iso.anchor.setTo(0.5, 0.3)
+  }
 
   create () {
     let width = this.game.width
     let height = this.game.height
 
     // Background
-    this.stage.background = this.add.sprite(0, 0, 'background')
-    this.stage.background.scale.setTo(4, 4)
+    this.stage.backgroundColor = '#03a9f4'
+    //this.stage.background = this.add.sprite(0, 0, 'background')
+    //this.stage.background.scale.setTo(4, 4)
     this.transparent = true
 
     // Title text
@@ -32,9 +36,27 @@ export default class extends Phaser.State {
     this.soundN = this.add.audio('soundN')
     nButton.onDown.addOnce(this.start, this)
     gButton.onDown.addOnce(this.start, this)
+
+    // Tiles
+    this.isoGroup = this.game.add.group()
+    this.spawnTiles()
+
   }
 
   start () {
     this.state.start('Game')
   }
+
+  spawnTiles () {
+    let tile
+    for (let xx = 0; xx < 180; xx += 38) {
+      for (let yy = 0; yy < 180; yy += 38) {
+      // Create a tile using the new game.add.isoSprite factory method at the specified position.
+      // The last parameter is the group you want to add it to (just like game.add.sprite)
+        tile = this.isoPlugin.addIsoSprite(xx, yy, 0, 'tile3', 0, this.isoGroup)
+        tile.anchor.set(0.5, 0)
+      }
+    }
+  }
+
 }
